dd7ecc656732862fb2cbd9062754276f953c09b9
jorge681jorge <gutierrez.jorge681@gmail.com>
2022-04-07 00:32:46 -0500

    config server
    configuración servidor
    
    -  constante servidor
    - puertos

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const StudentAPI = require('./dataSources/student_api');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        studentAPI: new StudentAPI() 
+    }),
+    introspection: true,
+    playground: true
+});
+
+server.listen(process.env.PORT || 4000).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
